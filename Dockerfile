1daab6df4550330f2cd9f545e3ce547f555f28a8
evgenijMerchansky <evgenij.merchansky@gmail.com>
2017-11-30 18:00:41 +0200

    added register and login functionality

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,18 @@
-# Dockerfile
 FROM node:6
 
-# Create app directory
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
-# Install app dependencies
 COPY package.json /usr/src/app/
+
 RUN npm install
 
-# Bundle app source
-COPY . /usr/src/app
+#RUN npm install pm2 -g
 
-# Build and optimize react app
-RUN npm run build
+COPY . /usr/src/app/
 
 EXPOSE 9000
 
-# defined in package.json
-CMD [ "npm", "run", "start:server" ]
+#CMD ["pm2-docker", "start-server"]
+
+CMD [ "npm", "run", "start-server" ]
--- server/app.js
+++ server/app.js
@@ -1,19 +1,39 @@
-// server/app.js
 const express = require('express');
-const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config/index');
 const path = require('path');
 
+// connect to the database and load models
+require(path.resolve(__dirname, 'models')).connect(config.dbUri);
+
 const app = express();
+// tell the app to look for static files in these directories
+app.use(express.static('./server/static/'));
+app.use(express.static('./client/dist/'));
+// tell the app to parse HTTP body messages
+app.use(bodyParser.urlencoded({ extended: false }));
+// pass the passport middleware
+app.use(passport.initialize());
 
-// Setup logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+// load passport strategies
+const localSignupStrategy = require(path.resolve(__dirname, 'passport', 'local-signup.js'));
+const localLoginStrategy = require(path.resolve(__dirname, 'passport', 'local-login.js'));
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
 
-// Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+// pass the authorization checker middleware
+const authCheckMiddleware = require(path.resolve(__dirname, 'middleware', 'auth-check.js'));
+app.use('/api', authCheckMiddleware);
 
-// Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+// routes
+const authRoutes = require(path.resolve(__dirname, 'routes', 'auth.js'));
+const apiRoutes = require(path.resolve(__dirname, 'routes', 'api.js'));
+app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
+
+
+// start the server
+app.listen(9000, () => {
+  console.log('Server is running on http://localhost:9000 or http://127.0.0.1:9000');
 });
-
-module.exports = app;
